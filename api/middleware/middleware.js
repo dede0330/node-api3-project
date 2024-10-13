d5cae6901cb21cecad385b6e2f75022d0005b498
dede0330 <deandre.bloomfield@gmail.com>
2024-10-12 17:16:17 -0700

    Done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,46 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+  );
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  Users.getById(req.params.id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: 'user not found' });
+      } else {
+        req.user = user;
+        next();
+      }
+    })
+    .catch(next);
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ message: 'missing required name field' });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const { text } = req.body;
+  if (!text) {
+    res.status(400).json({ message: 'missing required text field' });
+  } else {
+    next();
+  }
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
